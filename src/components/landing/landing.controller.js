6454d343e73b97380883372daa22166059125ee3
Daniel Klein <danhirschklein@gmail.com>
2016-07-11 16:13:52 -0600

    landing controller and carousel

---
+++ src/components/landing/landing.controller.js
@@ -0,0 +1,15 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('africaApp')
+    .controller('LandingCtrl', LandingCtrl);
+
+  HeaderCtrl.$inject = [];
+
+  function LandingCtrl() {
+    var vm = this;
+  }
+
+})();
