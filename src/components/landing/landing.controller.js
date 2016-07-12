fedf32eb39da221a8b0b37432aa7b0c55807709a
Daniel Klein <danhirschklein@gmail.com>
2016-07-11 18:58:35 -0600

    Created post format

--- src/components/landing/landing.controller.js
+++ src/components/landing/landing.controller.js
@@ -10,6 +10,43 @@
 
   function LandingCtrl() {
     var vm = this;
+    vm.posts = ["meow", "meow", "meow", "meow"];
+    // vm.posts = [
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fled across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   },
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fleeing across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   },
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fleeing across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   },
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fleeing across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   },
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fleeing across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   },
+    //   {
+    //     title: 'Man in Black',
+    //     description: 'Fleeing across the desrt',
+    //     image: 'http://www.trbimg.com/img-56722fc4/turbine/la-2446267-ca-1204-idris-elba-05-cmc-jpg-20151216/700/700x394'
+    //   }
+    // ];
+
+    vm.arr = [1,2,3,4,5];
+
   }
+
 
 })();
